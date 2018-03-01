94186f0c7cdbb6476e5348caeb1942fdca1f8d7b
alexseycc <36811793+alexseycc@users.noreply.github.com>
2018-03-01 07:09:00 -0300

    Delete GlassFishWeb (2).sh

--- GlassFishWeb (2).sh
+++
@@ -1,6 +0,0 @@
-apt-get update -y
-apt-get upgrade -y -f
-apt-get install default-jdk -y -f
-wget http://download.oracle.com/glassfish/4.1/release/glassfish-4.1.zip
-unzip glassfish-4.1.zip -d /opt
-cd /opt/glassfish4/bin && sh asadmin start-domain
